13c7137233d189acccc9c53fb566aa2bee246dd9
NowrinIslamMim <nowrin02@gmail.com>
2022-09-28 21:55:53 +0600

    Activity component added

--- src/Components/Activities/Activities.js
+++ src/Components/Activities/Activities.js
@@ -1,11 +1,24 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import Activity from '../Activity/Activity';
 import './Activities.css'
 
 const Activities = () => {
+
+    const [activities, setActivities] = useState([])
+
+    useEffect(() => {
+        fetch('./products.json')
+            .then(res => res.json())
+            .then(data => setActivities(data))
+    }, [])
     return (
-        <div className=''>
+        <div className='activity-container'>
             <div className='activities-Section'>
                 <h4>Select Activities for Today</h4>
+                {
+                    activities.map(activity => <Activity key={activity._id} activity={activity}></Activity>)
+                }
+
             </div>
             <div className='activity-list'>
                 <h4>List</h4>
---
+++ src/Components/Activity/Activity.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './Activity.css'
+
+const Activity = (props) => {
+    console.log(props);
+    return (
+        <div>
+
+        </div>
+    );
+};
+
+export default Activity;
